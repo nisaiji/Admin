417227e4f63a6ed0250db33a4303fc6f23813b5b
Nikhilesh1119 <nikhileshchouhan20685@acropolis.in>
2025-06-03 21:15:20 +0530

    add addstudent form

--- src/components/studentSetup/AddStudentForm.jsx
+++ src/components/studentSetup/AddStudentForm.jsx
@@ -7,9 +7,12 @@ import Spinner from "../Spinner";
 import EndPoints from "../../services/EndPoints";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
-import { FormControl, MenuItem, Select } from "@mui/material";
+import { FormControl, MenuItem, Select, TextField } from "@mui/material";
 import Breadcrumbs from "../BreadCrumbs";
 import REGEX from "../../utils/regix";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+import moment from "moment";
 
 const AddStudent = () => {
   const navigate = useNavigate();
@@ -67,11 +70,12 @@ const AddStudent = () => {
     if (!student.gender) return t("validationError.gender");
     if (!student.class) return t("validationError.class");
     if (!student.section) return t("validationError.section");
+    if (!student.address) return t("validationError.address");
     return "";
   };
 
   const capitalize = (str) =>
-  str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+    str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 
   const formik = useFormik({
     initialValues: {
@@ -82,15 +86,15 @@ const AddStudent = () => {
       phone: "",
       class: "",
       section: "",
-      // bloodGroup: "",
-      // dob: "",
-      // address: "",
-      // parentGender: "",
-      // parentAge: "",
-      // parentEmail: "",
-      // parentQualification: "",
-      // parentOccupation: "",
-      // parentAddress: "",
+      bloodGroup: "",
+      dob: "",
+      address: "",
+      parentGender: "",
+      parentAge: "",
+      parentEmail: "",
+      parentQualification: "",
+      parentOccupation: "",
+      parentAddress: "",
     },
     onSubmit: async (values) => {
       const e = validateData(values);
@@ -111,6 +115,19 @@ const AddStudent = () => {
           gender: values.gender,
           parentName: capitalize(values.parentName.trim()),
           phone: values.phone,
+          address: values.address,
+          ...(values.bloodGroup && { bloodGroup: values.bloodGroup }),
+          ...(values.dob && { dob: values.dob }),
+          ...(values.parentGender && { parentGender: values.parentGender }),
+          ...(values.parentAge && { parentAge: values.parentAge }),
+          ...(values.parentEmail && { parentEmail: values.parentEmail }),
+          ...(values.parentQualification && {
+            parentQualification: values.parentQualification,
+          }),
+          ...(values.parentOccupation && {
+            parentOccupation: values.parentOccupation,
+          }),
+          ...(values.parentAddress && { parentAddress: values.parentAddress }),
           sectionId: values.section,
         };
         const res = await axiosClient.post(
@@ -157,12 +174,34 @@ const AddStudent = () => {
   }, []);
 
   const options = [t("options.male"), t("options.female"), t("options.other")];
+  const bloodGroupOptions = ["A+", "A-", "B+", "B-", "O+", "O-", "AB+", "AB-"];
 
   const fields = [
     { name: "firstname", label: "First Name", type: "text" },
     { name: "lastname", label: "Last Name", type: "text" },
     { name: "parentName", label: "Parent Name", type: "text" },
     { name: "phone", label: "Phone Number", type: "text" },
+  ];
+
+  const optionalFields = [
+    { name: "address", label: "Address", optional: false },
+    { name: "bloodGroup", label: "Blood Group", optional: true },
+    { name: "dob", label: "Date of Birth", optional: true },
+    { name: "parentGender", label: "Parent Gender", optional: true },
+    { name: "parentAge", label: "Parent Age", optional: true },
+    {
+      name: "parentEmail",
+      label: "Parent Email",
+      type: "email",
+      optional: true,
+    },
+    {
+      name: "parentQualification",
+      label: "Parent Qualification",
+      optional: true,
+    },
+    { name: "parentOccupation", label: "Parent Occupation", optional: true },
+    { name: "parentAddress", label: "Parent Address", optional: true },
   ];
 
   const renderField = (name) => {
@@ -484,6 +523,156 @@ const AddStudent = () => {
             {renderGenderDropdown()}
             {renderClassDropdown()}
             {renderSectionDropdown()}
+          </div>
+          <div className="flex flex-wrap gap-4 mt-4">
+            {optionalFields.map((field) => (
+              <div
+                key={field.name}
+                className="flex flex-col flex-1 min-w-[500px]"
+              >
+                <label
+                  className={`font-semibold mb-1 ${
+                    isDarkMode ? "text-textPrimary" : "text-textBlack"
+                  }`}
+                >
+                  {field.label}{" "}
+                  {field?.optional && (
+                    <span className="text-sm text-gray-400">(Optional)</span>
+                  )}
+                </label>
+                {field.name === "bloodGroup" ||
+                field.name === "parentGender" ? (
+                  <FormControl
+                    fullWidth
+                    variant="outlined"
+                    sx={{
+                      border: "1px solid #2b2e4a80",
+                      borderRadius: "8px",
+                      backgroundColor: isDarkMode ? "" : "white",
+                      "& .MuiOutlinedInput-notchedOutline": {
+                        border: "none",
+                      },
+                      "& .MuiInputBase-root": {
+                        color: isDarkMode ? "#E3E8F3" : "black",
+                      },
+                      "& .MuiSvgIcon-root": {
+                        color: isDarkMode ? "#E3E8F3" : "black",
+                      },
+                    }}
+                  >
+                    <Select
+                      name={field.name}
+                      value={formik.values[field.name]}
+                      onChange={formik.handleChange}
+                      displayEmpty
+                      sx={{
+                        height: "40px",
+                        color:
+                          formik.values[field.name] === ""
+                            ? "gray"
+                            : isDarkMode
+                            ? "#E3E8F3"
+                            : "black",
+                      }}
+                      MenuProps={{
+                        PaperProps: {
+                          sx: {
+                            backgroundColor: isDarkMode ? "#1a1a1a" : "white",
+                            color: isDarkMode ? "#E3E8F3" : "black",
+                          },
+                        },
+                      }}
+                    >
+                      <MenuItem value="" disabled>
+                        {field.name === "bloodGroup"
+                          ? t("placeholders.bloodGroup")
+                          : t("placeholders.selectGender")}
+                      </MenuItem>
+                      {(field.name === "bloodGroup"
+                        ? bloodGroupOptions
+                        : options
+                      ).map((option) => (
+                        <MenuItem key={option} value={option}>
+                          {option}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                    {formik.touched[field.name] &&
+                      formik.errors[field.name] && (
+                        <div className="text-red-600 text-sm mt-1 ml-2">
+                          {formik.errors[field.name]}
+                        </div>
+                      )}
+                  </FormControl>
+                ) : field.name === "dob" ? (
+                  <LocalizationProvider dateAdapter={AdapterMoment}>
+                    <DatePicker
+                      views={["day", "month", "year"]}
+                      format="DD/MM/YYYY"
+                      value={
+                        formik.values[field.name]
+                          ? moment(formik.values[field.name], "DD/MM/YYYY")
+                          : null
+                      }
+                      maxDate={moment().startOf("day")}
+                      onChange={(date) => {
+                        if (date) {
+                          formik.setFieldValue(
+                            field.name,
+                            moment(date).format("DD/MM/YYYY")
+                          );
+                        }
+                      }}
+                      className={`w-full`}
+                      renderInput={(params) => (
+                        <TextField
+                          {...params}
+                          placeholder={t("placeholders.date")}
+                          variant="outlined"
+                        />
+                      )}
+                      sx={{
+                        width: "100%",
+                        height: "40px",
+                        border: "2px solid #2b2e4a80",
+                        borderRadius: "8px",
+                        backgroundColor: isDarkMode ? "" : "white",
+                        color: isDarkMode ? "#E3E8F3" : "black",
+                        "& .MuiOutlinedInput-root": {
+                          padding: 1,
+                          fontSize: "16px",
+                          minHeight: "40px",
+                          color: isDarkMode ? "#E3E8F3" : "black",
+                        },
+                        "& .MuiInputBase-input": {
+                          fontSize: "16px",
+                          padding: 1,
+                          height: "100%",
+                          color: isDarkMode ? "#E3E8F3" : "black",
+                        },
+                        "& .MuiSvgIcon-root": {
+                          color: isDarkMode ? "#E3E8F3" : "black",
+                        },
+                        "& .MuiOutlinedInput-notchedOutline": {
+                          border: "none",
+                        },
+                      }}
+                    />
+                  </LocalizationProvider>
+                ) : (
+                  <input
+                    type={field.type || "text"}
+                    name={field.name}
+                    placeholder={field.label}
+                    onChange={formik.handleChange}
+                    value={formik.values[field.name]}
+                    className={`border-2 border-borderLine bg-transparent rounded-lg pl-2 pr-10 py-1.5 w-full ${
+                      isDarkMode ? "text-textPrimary" : "text-textBlack"
+                    }`}
+                  />
+                )}
+              </div>
+            ))}
           </div>
 
           <div className="flex justify-end gap-4 mt-10 w-full">
